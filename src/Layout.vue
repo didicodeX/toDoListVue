9174c64f90631e61219e37f6077255c64f047fb7
Dylane <tanolamene@gmail.com>
2024-09-28 12:01:17 -0300

    Layout.vue

---
+++ src/Layout.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="layout">
+    <header>
+      <slot name="header"></slot>
+    </header>
+    <aside>
+      <slot name="aside"></slot>
+    </aside>
+    <main>
+      <slot name="main"></slot>
+    </main>
+    <footer>
+      <slot name="footer"></slot>
+    </footer>
+  </div>
+</template>
+
+<style>
+.layout{
+  display: grid;
+  grid-template-columns: 200px 1fr;
+}
+.layout > * {
+  border: 2px solid crimson;
+}
+.layout header,
+.layout footer {
+  grid-column: 1/-1;
+}
+</style>
