412f411b936c1605ce1e9ae61fc6da80377cfbe6
Dylane <tanolamene@gmail.com>
2024-09-28 12:00:51 -0300

    Button.vue

---
+++ src/Button.vue
@@ -0,0 +1,13 @@
+<template>
+  <button class="secondary"><slot></slot></button>
+</template>
+
+<script setup></script>
+
+<style>
+.secondary {
+  width: min-content;
+  width: 400px;
+  padding: 0px 1px;
+}
+</style>
